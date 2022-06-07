76f561afeca5f47c01a2284bc07553db6fe8ac62
molkex <molkex@icloud.com>
2022-06-07 14:47:29 +0300

    v0.0.2

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
+const backgroundColor = function(color){
+  document.querySelector('body').style.backgroundColor = color;}
+
+const guessMessage = function(message){
+  document.querySelector('.guess-message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function() {
   const guessingNumber = Number(document.querySelector('.number-input').value);
-  console.log(guessingNumber, typeof guessingNumber);
-
-  // No input
   if (!guessingNumber) {
-    document.querySelector('.guess-message').textContent = 'Введите чисто';
-
-    //  Player won
+    guessMessage('Введите чисто');
   } else if (guessingNumber === secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Правильно';
+    guessMessage('Правильно');
     document.querySelector('.question').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor =
-      'rgba(9, 250, 21, 1)';
+    backgroundColor('#09FA15FF')
     document.querySelector('.question').style.width = '50rem';
     if (score > highScore) {
       highScore = score;
@@ -25,49 +26,27 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   } else if (guessingNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.guess-message').textContent =
-        guessingNumber > secretNumber ? 'Слишком много' : 'Слишком мало';
+      guessMessage(guessingNumber > secretNumber ? 'Слишком много' : 'Слишком мало');
       score--;
       document.querySelector('.score').textContent = score;
+      backgroundColor('#FA0941FF')
+      setTimeout(`backgroundColor('#000000')`, 500);
     } else {
-      document.querySelector('.guess-message').textContent = 'Конец игры';
+      guessMessage('Конец игры');
       document.querySelector('.score').textContent = '0';
+      backgroundColor('#FA0941FF')
     }
   }
 });
 
-//   //  To high
-//   else if (guessingNumber > secretNumber) {
-//     if (score > 1) {
-//       document.querySelector('.guess-message').textContent = 'Слишком много';
-//       score--;
-//       document.querySelector('.score').textContent = score;
-//     } else {
-//       document.querySelector('.guess-message').textContent = 'Конец игры';
-//       document.querySelector('.score').textContent = '0';
-//     }
-//     //  To low
-//   } else if (guessingNumber < secretNumber) {
-//     if (score > 1) {
-//       document.querySelector('.guess-message').textContent = 'Слишком мало';
-//       score--;
-//       document.querySelector('.score').textContent = score;
-//     } else {
-//       document.querySelector('.guess-message').textContent = 'Конец игры';
-//       document.querySelector('.score').textContent = '0';
-//     }
-//   }
-// })
-
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector('.again').addEventListener('click', function() {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 
   document.querySelector('.question').style.width = '25rem';
   document.querySelector('.question').textContent = '???';
-  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
-  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  backgroundColor('#000000')
+  guessMessage('Начни угадывать!');
   document.querySelector('.score').textContent = score;
-  // document.querySelector('.number-input').textContent = ' ';
   document.querySelector('.number-input').value = '';
 });
